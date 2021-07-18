843abc957254ee5e2add5dd789a88508e7fd971c
Elechim01 <68447158+Elechim01@users.noreply.github.com>
2021-07-18 15:50:43 +0200

    Initial Commit

---
+++ PushMessaging/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PushMessaging
+//
+//  Created by Michele Manniello on 18/07/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PushMessaging/PushMessagingApp.swift
@@ -0,0 +1,17 @@
+//
+//  PushMessagingApp.swift
+//  PushMessaging
+//
+//  Created by Michele Manniello on 18/07/21.
+//
+
+import SwiftUI
+
+@main
+struct PushMessagingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
